a4ffbf62e6ab2b42c6bd0c34877f253ee097f18d
aleniaa <mannilenia@gmail.com>
2023-04-23 00:01:58 +0200

    gestione utenti completa

--- src/app/scheda-utenti/scheda-utenti.component.ts
+++ src/app/scheda-utenti/scheda-utenti.component.ts
@@ -13,6 +13,7 @@ export class SchedaUtentiComponent implements OnInit{
   
   public utenti: Utente[] = [];
   public modificaUtente!: Utente; //il punto esclamativo serve a non dover inizializzare la variabile per forza
+  public deleteUtente!: Utente;
 
   constructor(private utenteService: UtenteService) { }
 
@@ -33,15 +34,36 @@ export class SchedaUtentiComponent implements OnInit{
     )
   }
 
-/*   public findUtente(): void{
-    this.utenteService.trovaUtente().subscribe(
-
-      (response: Utente[])=>{this.utenti = response;}
-    ,
-
-    ) 
+  public onOpenModal(utente: Utente, mode: string): void { //questo metodo serve ad aprire i modal in base ai click
+    const button = document.createElement('button');
+    const container= document.getElementById('main-container');
     
-  }*/
+    button.type= 'button';
+    button.style.display= 'none';
+    button.setAttribute('data-toggle','modal');
+
+    if(mode === 'add' ){
+      button.setAttribute('data-target','#aggiungiUtenteModal');
+
+    }
+
+    if(mode === 'edit' ){
+      this.modificaUtente = utente; //this vuol dire l'utente in questa classe
+      button.setAttribute('data-target','#aggiornaUtenteModal');
+
+    }
+    if(mode === 'delete'){
+      this.deleteUtente = utente;
+      button.setAttribute('data-target','#cancellaUtenteModal');
+
+    }
+  
+    container?.appendChild(button);
+    
+    button.click();
+
+
+  }
 
   public onAggiungiUtente(addForm: NgForm): void{
     document.getElementById('add-employee-form')?.click();
@@ -58,43 +80,7 @@ export class SchedaUtentiComponent implements OnInit{
     ); //addform.value restituisce una rappresentaione json dei dati inseriti nel form
   }
 
-  public onOpenModal(utente: Utente, mode: string): void { //questo metodo serve ad aprire i modal in base ai click
-    const button = document.createElement('button');
-    const container= document.getElementById('main-container');
-    
-    button.type= 'button';
-    button.style.display= 'none';
-    button.setAttribute('data-toggle','modal');
-    if(mode === 'edit'){
-      this.modificaUtente = utente; //this vuol dire l'utente in questa classe
-      button.setAttribute('data-target','#aggiornaUtenteModal');
 
-    }
-    if(mode === 'delete'){
-      button.setAttribute('data-target','#cancellaUtenteModal');
-
-    }
-
-    container?.appendChild(button);
-    
-    button.click();
-
-
-  }
-
-  public onClickAggiungiUtente(): void {
-    const button = document.createElement('button');
-    const aggiungiUtenteButton = document.getElementById('aggiungiUtenteButton');
-    button.type= 'button';
-    button.className= "btn btn-primary";
-    button.style.display= 'none';
-    button.setAttribute('data-toggle','modal');
-    button.setAttribute('data-target','#aggiungiUtenteModal');
-    aggiungiUtenteButton?.appendChild(button);
-    button.click();
-
-
-  }
 
   public aggiornaUtente(utente: Utente): void{
     this.utenteService.aggiornaUtente(utente).subscribe(
@@ -106,6 +92,43 @@ export class SchedaUtentiComponent implements OnInit{
         alert(error.message);
       },
     ); //addform.value è una rappresentaione json dei dati inseriti nel form
+  }
+
+  public cancellaUtente(utenteId: number): void{
+    this.utenteService.cancellaUtente(utenteId).subscribe(
+      (response: void) => { //jfoiewfjwoiej
+        console.log(response);
+        this.getUtenti();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      },
+    ); //addform.value è una rappresentaione json dei dati inseriti nel form
+  }
+
+  public cercaUtenti(key: string): void{
+    const risultati: Utente[]=[];
+    for(const utente of this.utenti){
+      if(utente.nome.toLocaleLowerCase().indexOf(key.toLowerCase()) !==-1
+      || utente.cognome.toLocaleLowerCase().indexOf(key.toLowerCase()) !==-1
+      || utente.telefono.toLocaleLowerCase().indexOf(key.toLowerCase()) !==-1
+      ){
+        risultati.push(utente);
+      }
+    }
+
+    this.utenti= risultati;
+/*      if((risultati.length===0 || !key) && key===""){
+      this.getUtenti();
+    }  */
+
+    if(risultati.length===0 || !key) {
+      if(key===""){
+        this.getUtenti();
+      }
+      
+    } 
+
   }
 
 }
