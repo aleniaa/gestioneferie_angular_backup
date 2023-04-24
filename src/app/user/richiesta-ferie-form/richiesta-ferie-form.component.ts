465a9a861a8751ccb05c6584544211a7601d53d5
ilenia mannino <ilenia.mannino@vr.dipvvf.it>
2023-04-24 17:58:28 +0200

    iniziato a scrivere il form ferie

--- src/app/user/richiesta-ferie-form/richiesta-ferie-form.component.ts
+++ src/app/user/richiesta-ferie-form/richiesta-ferie-form.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class RichiestaFerieFormComponent {
 
+public toggleForm(form: string): void{
+
+  var x = document.getElementById("permesso_form");
+
+  if(form === 'congedo' ){
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+
+  }
+
+  if(form === 'permesso_breve' ){
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+
+  }
+  if(form === 'recupero_ore'){
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+
+  }
+}
+
 }
