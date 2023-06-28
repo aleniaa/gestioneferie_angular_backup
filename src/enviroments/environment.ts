2a3d3d5cdbbf6b0c8a9f44c46aa2ca2b9709ad28
ilenia mannino <ilenia.mannino@vr.dipvvf.it>
2023-06-28 17:18:15 +0200

    cominciato a cambiare richiesta ferie

--- src/enviroments/environment.ts
+++ src/enviroments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: false,
-    apiBaseUrl: 'http://172.20.10.3:8082'
-    //apiBaseUrl: 'http://localhost:8082'
+    //apiBaseUrl: 'http://172.20.10.3:8082'
+    apiBaseUrl: 'http://localhost:8082'
 };
